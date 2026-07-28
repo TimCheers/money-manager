30c2c6f5b38ceba5ac2fbf2b54344ffa7be6c47f
TimCheers <timcheerson@gmail.com>
2026-07-28 16:50:58 +0900

    feat: add MongoDB connection via Mongoose

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
+import "dotenv/config"; // сразу читает .env и заполняет process.env
 import app from "./src/app.js";
+import connectDB from "./src/config/db.js";
 
 const PORT = 3000;
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+
+connectDB().then(() => {
+    app.listen(PORT, () => {
+        console.log(`Server is running on http://localhost:${PORT}`);
+    });
 });
