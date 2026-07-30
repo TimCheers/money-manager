2ec17eaa49e01c4a07788f16cf09207036b17357
TimCheers <timcheerson@gmail.com>
2026-07-30 18:56:54 +0900

    Render transaction list via map()

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,13 +4,20 @@ import TransactionItem from "./components/TransactionItem.jsx"
 
 function App() {
   const [count, setCount] = useState(0)
+  const transactions = [
+    { id: 1, amount: 500, category: "Food", date: "2026-07-29" },
+    { id: 2, amount: 32423, category: "Games", date: "2026-07-13" },
+    { id: 3, amount: 5045340, category: "Tax", date: "2026-07-03" },
+    { id: 4, amount: 32421, category: "Food", date: "2026-07-23" },
 
+  ];
   return (
     <>
       <section id="center">
         <h1>Money Manager </h1>
-        <TransactionItem amount={500} category="Food" date="2026-07-29" />
-        <TransactionItem amount={1000} category="Tax" date="2026-07-30" />
+        {transactions.map((transaction) => (
+          <TransactionItem key={transaction.id} amount={transaction.amount} category={transaction.category} date={transaction.date} />
+        ))}
       </section>
     </>
   )
