b899bae7685a4f3e0784523aa306be317c13052c
TimCheers <timcheerson@gmail.com>
2026-09-21 21:13:10 +0900

    feat(auth): add Register page with auto-login after signup

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,6 +1,75 @@
+import { useState } from "react";
+import { useAuth } from "../context/AuthContext.jsx";
+import { useNavigate } from "react-router-dom";
+
 function Register() {
+  const [username, setUsername] = useState("");
+  const [dateOfBirth, setDateOfBirth] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const { login } = useAuth();
+  const navigate = useNavigate();
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+    try {
+      const response = await fetch("http://localhost:3000/auth/register", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ username, dateOfBirth, email, password }),
+      })
+
+      const data = await response.json();
+
+      if (!response.ok) {
+        console.error("Ошибка:", data.error);
+        return;
+      }
+
+      const loginResponse = await fetch("http://localhost:3000/auth/login", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ email, password }),
+      })
+
+      const loginData = await loginResponse.json();
+
+      if (!loginResponse.ok) {
+        console.error("Ошибка:", loginData.error);
+        return;
+      }
+
+      login(loginData.token);
+      navigate("/");
+    } catch (error) {
+      console.error("Сетевая ошибка:", error);
+    }
+  }
+
   return (
-    <p>Register</p>
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        value={username}
+        onChange={(event) => setUsername(event.target.value)}
+      />
+      <input
+        type="date"
+        value={dateOfBirth}
+        onChange={(event) => setDateOfBirth(event.target.value)}
+      />
+      <input
+        type="text"
+        value={email}
+        onChange={(event) => setEmail(event.target.value)}
+      />
+      <input
+        type="password"
+        value={password}
+        onChange={(event) => setPassword(event.target.value)}
+      />
+      <button type="submit">Ввод</button>
+    </form>
   );
 }
 
